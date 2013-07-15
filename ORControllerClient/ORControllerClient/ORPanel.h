4360ba6636eb2b4630509c3551499d179bdb125b
Eric Bariaux <eric@openremote.org>
2013-07-15 16:51:27 +0200

    Added object to represent a Panel

---
+++ ORControllerClient/ORControllerClient/ORPanel.h
@@ -0,0 +1,18 @@
+//
+//  ORPanel.h
+//  ORControllerClient
+//
+//  Created by Eric Bariaux on 15/07/13.
+//  Copyright (c) 2013 OpenRemote. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ORPanel : NSObject
+
+/**
+ * The name of the panel.
+ */
+@property (strong, nonatomic) NSString *name;
+
+@end
---
+++ ORControllerClient/ORControllerClient/ORPanel.m
@@ -0,0 +1,13 @@
+//
+//  ORPanel.m
+//  ORControllerClient
+//
+//  Created by Eric Bariaux on 15/07/13.
+//  Copyright (c) 2013 OpenRemote. All rights reserved.
+//
+
+#import "ORPanel.h"
+
+@implementation ORPanel
+
+@end
