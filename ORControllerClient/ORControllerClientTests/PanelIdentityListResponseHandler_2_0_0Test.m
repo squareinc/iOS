378b6508a71de8f82888d558e1cf9ad8b996810f
Eric Bariaux <eric@openremote.org>
2013-08-06 14:59:37 +0200

    Added unit tests for PanelIdentityListResponseHandler_2_0_0

---
+++ ORControllerClient/ORControllerClientTests/PanelIdentityListResponseHandler_2_0_0Test.m
@@ -0,0 +1,56 @@
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2013, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#import "PanelIdentityListResponseHandler_2_0_0Test.h"
+#import "PanelIdentityListResponseHandler_2_0_0.h"
+#import "ORPanelsParserTest.h"
+#import "ORPanel.h"
+
+@interface ORPanelsParserTest ()
+
+- (void)assertValidResponse:(id)panels;
+
+@end
+
+@implementation PanelIdentityListResponseHandler_2_0_0Test
+
+- (void)testSuccessfulResponse
+{
+    __block BOOL successHandlerCalled = NO;
+    void (^successHandler)(NSArray *) = ^(NSArray *panels) {
+        successHandlerCalled = YES;
+        [[[ORPanelsParserTest alloc] init] assertValidResponse:panels];
+    };
+    PanelIdentityListResponseHandler_2_0_0 *responseHandler = [[PanelIdentityListResponseHandler_2_0_0 alloc]
+                                                               initWithSuccessHandler:successHandler
+                                                               errorHandler:^(NSError *error) {
+                                                                   STFail(@"No error should be reported");
+                                                               }];
+    
+    NSURL *url = [[NSBundle bundleForClass:[self class]] URLForResource:@"RequestPanelIdentityListValidResponse" withExtension:@"xml"];
+    NSData *data = [NSData dataWithContentsOfURL:url];
+    
+    [responseHandler connectionDidFinishLoading:nil receivedData:data];
+    
+    STAssertTrue(successHandlerCalled, @"Success handler should have been called upon receiving response with valid data");
+}
+
+@end
