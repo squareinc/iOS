66adadf1fd776fcc29a33b6a0e42dc7603ce9c9b
Eric Bariaux <eric@openremote.org>
2014-01-24 15:28:46 +0100

    Added new model object for sensor

---
+++ ORControllerClient/ORControllerClient/ORSensor.h
@@ -0,0 +1,46 @@
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2014, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#import <Foundation/Foundation.h>
+
+@class ORObjectIdentifier;
+
+/**
+ * Model object representing a sensor in the OR UI model domain.
+ * The only information available at this stage is the sensor identifier.
+ *
+ * Sensor states are specific to the use of a sensor by a UI widget
+ * and are taken care of by the ORSensorStatesMapping and ORSensorLink classes.
+ */
+@interface ORSensor : NSObject
+
+/**
+ * Initializes the sensor with the given identifier.
+ *
+ * @param anIdentifier ORObjectIdenfitier to use as identifier for this sensor
+ *
+ * @return An ORSensor object initialized with the provided identifier.
+ */
+- (id)initWithIdentifier:(ORObjectIdentifier *)anIdentifier;
+
+@property (nonatomic, strong, readonly) ORObjectIdentifier *identifier;
+
+@end
---
+++ ORControllerClient/ORControllerClient/ORSensor.m
@@ -0,0 +1,43 @@
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2014, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#import "ORSensor.h"
+
+@interface ORSensor ()
+
+@property (nonatomic, strong, readwrite) ORObjectIdentifier *identifier;
+
+@end
+
+@implementation ORSensor
+
+- (id)initWithIdentifier:(ORObjectIdentifier *)anIdentifier
+{
+    self = [super init];
+    if (self) {
+        self.identifier = anIdentifier;
+    }
+    return self;
+}
+
+@synthesize identifier;
+
+@end
