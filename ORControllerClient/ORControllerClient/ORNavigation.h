ccaee7a78e31ef726d0473bf5b6f5849f1969372
Eric Bariaux <eric@openremote.org>
2014-03-01 16:28:08 +0100

    Started adding new ORNavigation model object to replace older Navigate

---
+++ ORControllerClient/ORControllerClient/ORNavigation.h
@@ -0,0 +1,41 @@
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2014, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#import <Foundation/Foundation.h>
+
+/**
+ * Enumeration indicating type of navigation.
+ */
+typedef NS_ENUM(NSInteger, ORNavigationType) {
+    ORNavigationToGroupOrScreen,
+    ORNavigationPreviousScreen,
+    ORNavigationNextScreen,
+    ORNavigationBack,
+    ORNavigationSettings,
+    ORNavigationLogin,
+    ORNavigationLogout
+};
+
+@interface ORNavigation : NSObject
+
+@property (nonatomic, readonly) ORNavigationType navigationType;
+
+@end
