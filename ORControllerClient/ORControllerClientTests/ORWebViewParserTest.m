c2e86e80be36ecb80b2ff5a70f2fc39be3beca5b
Eric Bariaux <ebariaux@tinsys.com>
2014-03-17 11:30:06 +0100

    Added unit test for ORWebView parser

---
+++ ORControllerClient/ORControllerClientTests/ORWebViewParserTest.m
@@ -0,0 +1,93 @@
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2014, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#import "ORWebViewParserTest.h"
+#import "DefinitionElementParserRegister.h"
+#import "XMLEntity.h"
+#import "DefinitionParserMock.h"
+#import "ORWebViewParser.h"
+#import "ORSensorLinkParser.h"
+#import "ORSensorStateParser.h"
+#import "ORObjectIdentifier.h"
+
+@implementation ORWebViewParserTest
+
+- (DefinitionElementParser *)parseXMLSnippet:(NSString *)snippet
+{
+    DefinitionElementParserRegister *depRegistry = [[DefinitionElementParserRegister alloc] init];
+    [depRegistry registerParserClass:[ORWebViewParser class] endSelector:@selector(setTopLevelParser:) forTag:WEB];
+    [depRegistry registerParserClass:[ORSensorLinkParser class] endSelector:@selector(endSensorLinkElement:) forTag:LINK];
+    [depRegistry registerParserClass:[ORSensorStateParser class] endSelector:@selector(endSensorStateElement:) forTag:STATE];
+    
+    NSXMLParser *xmlParser = [[NSXMLParser alloc] initWithData:[snippet dataUsingEncoding:NSUTF8StringEncoding]];
+    DefinitionParserMock *parser = [[DefinitionParserMock alloc] initWithRegister:depRegistry attributes:nil];
+    [parser addKnownTag:WEB];
+    [xmlParser setDelegate:parser];
+    [xmlParser parse];
+    
+    return parser.topLevelParser;
+}
+
+- (ORWebView *)parseValidXMLSnippet:(NSString *)snippet
+{
+    DefinitionElementParser *topLevelParser = [self parseXMLSnippet:snippet];
+    STAssertNotNil(topLevelParser, @"Valid XML snippet should be parsed correctly");
+    STAssertTrue([topLevelParser isMemberOfClass:[ORWebViewParser class]], @"Parser used should be an ORWebViewParser");
+    ORWebView *webView = ((ORWebViewParser *)topLevelParser).web;
+    STAssertNotNil(webView, @"A web view should be parsed for given XML snippet");
+    
+    return webView;
+}
+
+- (void)testParseWebViewNoUserNoSensor
+{
+    ORWebView *webView = [self parseValidXMLSnippet:@"<web id=\"10\" src=\"url\"/>"];
+    
+    STAssertEqualObjects([[ORObjectIdentifier alloc] initWithIntegerId:10], webView.identifier, @"Parsed web view should have 10 as identifer");
+    
+    STAssertEqualObjects(webView.src, @"url", @"Parsed web view should have 'url' as src property");
+    STAssertNil(webView.username, @"Parsed web view should not have a user name defined");
+    STAssertNil(webView.password, @"Parsed web view should not have a password defined");
+}
+
+- (void)testParseWebViewUsernamePasswordNoSensor
+{
+    ORWebView *webView = [self parseValidXMLSnippet:@"<web id=\"10\" src=\"url\" username=\"usr\" password=\"pwd\"/>"];
+    
+    STAssertEqualObjects([[ORObjectIdentifier alloc] initWithIntegerId:10], webView.identifier, @"Parsed web view should have 10 as identifer");
+    
+    STAssertEqualObjects(webView.src, @"url", @"Parsed web view should have 'url' as src property");
+    STAssertEqualObjects(webView.username, @"usr", @"Parsed web view should have 'usr' as username");
+    STAssertEqualObjects(webView.password, @"pwd", @"Parsed web view should have 'pwd' as password");
+}
+
+- (void)testParseWebViewUsernamePasswordWithSensor
+{
+    ORWebView *webView = [self parseValidXMLSnippet:@"<web id=\"10\" src=\"url\" username=\"usr\" password=\"pwd\"><link type=\"sensor\" ref=\"3\"/></web>"];
+    
+    STAssertEqualObjects([[ORObjectIdentifier alloc] initWithIntegerId:10], webView.identifier, @"Parsed web view should have 10 as identifer");
+    
+    STAssertEqualObjects(webView.src, @"url", @"Parsed web view should have 'url' as src property");
+    STAssertEqualObjects(webView.username, @"usr", @"Parsed web view should have 'usr' as username");
+    STAssertEqualObjects(webView.password, @"pwd", @"Parsed web view should have 'pwd' as password");
+}
+
+@end
