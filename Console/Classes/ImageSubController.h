64abbd481a1fb4b963140df82c5d5ebc8bf12819
Eric Bariaux <ebariaux@tinsys.com>
2014-03-20 18:30:23 +0100

    Got rid of SensorySubController class, not used anymore, all value update via sensors is handled by client library and used via KVO

--- Console/Classes/ImageSubController.h
+++ Console/Classes/ImageSubController.h
@@ -18,8 +18,8 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-#import "SensorySubController.h"
+#import "ComponentSubController.h"
 
-@interface ImageSubController : SensorySubController
+@interface ImageSubController : ComponentSubController
 
 @end
--- Console/Classes/WebSubController.h
+++ Console/Classes/WebSubController.h
@@ -18,8 +18,8 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-#import "SensorySubController.h"
+#import "ComponentSubController.h"
 
-@interface WebSubController : SensorySubController
+@interface WebSubController : ComponentSubController
 
 @end
